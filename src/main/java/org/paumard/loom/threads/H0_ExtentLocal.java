c84ab92b6d8849b0f0b91f235a2085ba1b2c1889
DB <3823705+dineshbhagat@users.noreply.github.com>
2023-04-09 15:30:08 +0530

    Update H0_ExtentLocal.java

--- src/main/java/org/paumard/loom/threads/H0_ExtentLocal.java
+++ src/main/java/org/paumard/loom/threads/H0_ExtentLocal.java
@@ -1,21 +1,22 @@
-package org.paumard.loom.threads;
-
-import jdk.incubator.concurrent.ExtentLocal;
-
-public class H0_ExtentLocal {
-
-    // --enable-preview --add-modules jdk.incubator.concurrent --add-exports java.base/jdk.internal.vm=ALL-UNNAMED
-
-    public static void main(String[] args) throws InterruptedException {
-
-        ExtentLocal<String> KEY = ExtentLocal.newInstance();
-
-        Runnable task = () -> System.out.println("Key is = " + KEY.get());
-
-        Runnable taskWithScopeLocalA = () -> ExtentLocal.where(KEY, "Value A").run(task);
-        Runnable taskWithScopeLocalB = () -> ExtentLocal.where(KEY, "Value B").run(task);
-
-        taskWithScopeLocalA.run();
-        taskWithScopeLocalB.run();
-    }
-}
+//package org.paumard.loom.threads;
+//
+//import jdk.incubator.concurrent.ExtentLocal;
+//
+//public class H0_ExtentLocal {
+//
+//    // --enable-preview --add-modules jdk.incubator.concurrent --add-exports java.base/jdk.internal.vm=ALL-UNNAMED
+//
+//    public static void main(String[] args) throws InterruptedException {
+//
+//        ExtentLocal<String> KEY = ExtentLocal.newInstance();
+//
+//        Runnable task = () -> System.out.println("Key is = " + KEY.get());
+//
+//        Runnable taskWithScopeLocalA = () -> ExtentLocal.where(KEY, "Value A").run(task);
+//        Runnable taskWithScopeLocalB = () -> ExtentLocal.where(KEY, "Value B").run(task);
+//
+//        taskWithScopeLocalA.run();
+//        taskWithScopeLocalB.run();
+//    }
+//}
+//NOTE: Commented since I was using java 20 which does not have class `jdk.incubator.concurrent.ExtentLocal`
